760451359ca1a82952cf4b9da5d5318a52b252e7
imskanand <the.anand3099@gmail.com>
2022-08-18 16:52:04 +0530

    Added Comment in the File

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -3,35 +3,36 @@
  *
  * [2] Add Two Numbers
  */
-
+#include "leetcode_template.h"
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-      ListNode *dummyHead = new ListNode();
-      ListNode *curr = dummyHead;
-      int carry = 0;
-      while (l1 != NULL || l2 != NULL || carry != 0) {
-        int x = l1 ? l1->val : 0;
-        int y = l2 ? l2->val : 0;
-        int sum = x + y + carry;
-        carry = sum / 10;
-        curr->next = new ListNode(sum % 10);
-        curr = curr->next;
-        l1 = l1 ? l1->next : nullptr;
-        l2 = l2 ? l2->next : nullptr;
-      }
-      return dummyHead->next;
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    // Creating a list node
+    ListNode *dummyHead = new ListNode();
+    ListNode *curr = dummyHead;
+    int carry = 0;
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+      // Check if we have value or if list end assign it to 0
+      int x = l1 ? l1->val : 0;
+      int y = l2 ? l2->val : 0;
+      int sum = x + y + carry;
+      carry = sum / 10;
+      curr->next = new ListNode(sum % 10);
+      curr = curr->next;
+      l1 = l1 ? l1->next : nullptr;
+      l2 = l2 ? l2->next : nullptr;
     }
+    return dummyHead->next;
+  }
 };
 // @lc code=end
